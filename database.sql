91c5213effb55a6429128e2613e5ca04fc1e2dcf
Janet Scanlon <janet.Lscanlon@gmail.com>
2024-05-20 11:37:03 -0500

    added required callback function for req.logout after passport update

--- database.sql
+++ database.sql
@@ -2,8 +2,18 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+DROP TABLE "user";
+
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "first_name" VARCHAR (80),
+    "pronouns" VARCHAR (20),
+    "friends_list" VARCHAR (100)
 );
+
+INSERT INTO "user" ("username", "password", "first_name", "pronouns") 
+VALUES
+('usertest', 'passwordtest', 'first name', 'she/they');
+
