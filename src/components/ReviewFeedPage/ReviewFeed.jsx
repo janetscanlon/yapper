8c314e3221483b24d99092e5f8cef0b0e55e0fc7
Janet Scanlon <janet.Lscanlon@gmail.com>
2024-05-22 10:35:09 -0500

    sucessfully rendered reviews info with reviews.map

--- src/components/ReviewFeedPage/ReviewFeed.jsx
+++ src/components/ReviewFeedPage/ReviewFeed.jsx
@@ -6,6 +6,7 @@ import ReviewPost from '../ReviewPost/ReviewPost'
 
 function ReviewFeed() {
     const dispatch = useDispatch()
+    const reviews = useSelector((store) => store.reviews)
 
     useEffect(() => {
         dispatch({ type: 'GET_REVIEWS'})
@@ -20,6 +21,14 @@ function ReviewFeed() {
         <h3>
             Review Post component here
         </h3>
+        {reviews.map(review => {
+            return(
+                <div key={review.id}>
+                    <h3>{review.book_title} by {review.book_author}</h3>
+                    <p>{review.review_input}</p>
+                </div>
+            )
+        })}
         <ReviewPost/>
         </div>
     )
