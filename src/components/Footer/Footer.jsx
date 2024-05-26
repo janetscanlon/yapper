f6255d55c637da23a6188b5609d6e6f7a328f9c8
Janet Scanlon <janet.Lscanlon@gmail.com>
2024-05-26 12:00:37 -0500

    additional commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ import './Footer.css';
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 function Footer() {
-  return <footer>&copy; Janet Scanlon</footer>;
+  return <footer>&copy; Yapper 2024</footer>;
 }
 
 export default Footer;
