e5ac31693b6cda8424b5b9ce917d466e2713e882
Janet Scanlon <janet.Lscanlon@gmail.com>
2024-07-17 12:45:45 -0500

    modified landing page text

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -20,36 +20,19 @@ function LandingPage() {
       <div className="grid">
         <div className="grid-col grid-col_8">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-            id felis metus. Vestibulum et pulvinar tortor. Morbi pharetra lacus
-            ut ex molestie blandit. Etiam et turpis sit amet risus mollis
-            interdum. Suspendisse et justo vitae metus bibendum fringilla sed
-            sed justo. Aliquam sollicitudin dapibus lectus, vitae consequat odio
-            elementum eget. Praesent efficitur eros vitae nunc interdum, eu
-            interdum justo facilisis. Sed pulvinar nulla ac dignissim efficitur.
-            Quisque eget eros metus. Vestibulum bibendum fringilla nibh a
-            luctus. Duis a sapien metus.
-          </p>
+          Welcome to <b>Yapper</b>, your dedicated social media platform for book lovers! A community to share your passion for reading, connect with friends, and discover new books.
 
-          <p>
-            Praesent consectetur orci dui, id elementum eros facilisis id. Sed
-            id dolor in augue porttitor faucibus eget sit amet ante. Nunc
-            consectetur placerat pharetra. Aenean gravida ex ut erat commodo, ut
-            finibus metus facilisis. Nullam eget lectus non urna rhoncus
-            accumsan quis id massa. Curabitur sit amet dolor nisl. Proin
-            euismod, augue at condimentum rhoncus, massa lorem semper lacus, sed
-            lobortis augue mi vel felis. Duis ultrices sapien at est convallis
-            congue.
           </p>
+        <b>Key Features</b>
+          <ul>
+          
 
-          <p>
-            Fusce porta diam ac tortor elementum, ut imperdiet metus volutpat.
-            Suspendisse posuere dapibus maximus. Aliquam vitae felis libero. In
-            vehicula sapien at semper ultrices. Vivamus sed feugiat libero. Sed
-            sagittis neque id diam euismod, ut egestas felis ultricies. Nullam
-            non fermentum mauris. Sed in enim ac turpis faucibus pretium in sit
-            amet nisi.
-          </p>
+            <li> <b>Create an Account:</b> Sign up easily to start sharing your book reviews and connecting with fellow readers.</li>
+            <li> <b>Post Reviews:</b> Share insightful reviews of your recent reads to help others discover their next favorite book.</li>
+            <li> <b>Follow Friends:</b> Stay updated with your friends' reading journeys by following them, liking their reviews, and engaging in lively discussions.</li>
+            <li> <b>Interactive Features:</b>Engage with the community by liking your friends' reviews, and look forward to leaving comments on reviews—a new feature coming soon!</li>
+
+        </ul>
         </div>
         <div className="grid-col grid-col_4">
           <RegisterForm />
